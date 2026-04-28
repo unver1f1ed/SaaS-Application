34652234efeb8a8fb147d2ba018991dca7936fa6
Bohdan <Bohdanpaslavskyi@gmail.com>
2026-04-28 23:48:53 +0300

    feature: added navigation

--- SaaS-PR/Core/NavigationService.cs
+++ SaaS-PR/Core/NavigationService.cs
@@ -9,12 +9,18 @@ public class NavigationService : ViewModelBase
 {
     private readonly Func<Type, ViewModelBase> _viewModelFactory;
     private ViewModelBase? _currentView;
+    private readonly List<ViewModelBase> _history = new();
+    private int _historyIndex = -1;
 
     public ViewModelBase? CurrentView
     {
         get => this._currentView;
         private set => this.SetProperty(ref this._currentView, value);
     }
+
+    public bool CanGoBack => this._historyIndex > 0;
+
+    public bool CanGoForward => this._historyIndex < this._history.Count - 1;
 
     public NavigationService(Func<Type, ViewModelBase> viewModelFactory)
     {
@@ -24,6 +30,54 @@ public class NavigationService : ViewModelBase
     public void NavigateTo<TViewModel>()
         where TViewModel : ViewModelBase
     {
-        this.CurrentView = this._viewModelFactory(typeof(TViewModel));
+        var viewModel = this._viewModelFactory(typeof(TViewModel));
+
+        // If we navigated back and then navigate to a new view, truncate forward history
+        if (this._historyIndex < this._history.Count - 1)
+        {
+            this._history.RemoveRange(this._historyIndex + 1, this._history.Count - this._historyIndex - 1);
+        }
+
+        // Add to history (avoid duplicates at consecutive positions)
+        if (this._history.Count == 0 || this._history[^1].GetType() != typeof(TViewModel))
+        {
+            this._history.Add(viewModel);
+            this._historyIndex++;
+        }
+        else
+        {
+            // Replace the current history entry with the new instance
+            this._history[this._historyIndex] = viewModel;
+        }
+
+        this.CurrentView = viewModel;
+        this.OnPropertyChanged(nameof(this.CanGoBack));
+        this.OnPropertyChanged(nameof(this.CanGoForward));
+    }
+
+    public void GoBack()
+    {
+        if (!this.CanGoBack)
+        {
+            return;
+        }
+
+        this._historyIndex--;
+        this.CurrentView = this._history[this._historyIndex];
+        this.OnPropertyChanged(nameof(this.CanGoBack));
+        this.OnPropertyChanged(nameof(this.CanGoForward));
+    }
+
+    public void GoForward()
+    {
+        if (!this.CanGoForward)
+        {
+            return;
+        }
+
+        this._historyIndex++;
+        this.CurrentView = this._history[this._historyIndex];
+        this.OnPropertyChanged(nameof(this.CanGoBack));
+        this.OnPropertyChanged(nameof(this.CanGoForward));
     }
 }
